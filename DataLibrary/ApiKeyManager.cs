c799994e2be17cf6d716b4b6c6e881226f7b9c93
Tomasz <urban776@gmail.com>
2020-11-18 14:42:20 +0100

    Add Generation and storing ApiKeys along with Db project, Enable Swagger Xml documentation, move connection string to secrets

---
+++ DataLibrary/ApiKeyManager.cs
@@ -0,0 +1,53 @@
+﻿using DataLibrary.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Text;
+using Dapper;
+
+namespace DataLibrary
+{
+    public class ApiKeyManager : IApiKeyManager
+    {
+        private readonly IConfiguration _config;
+
+        public ApiKeyManager(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        public string GenerateKey()
+        {
+            var key = new byte[32];
+            using (var generator = RandomNumberGenerator.Create())
+                generator.GetBytes(key);
+            string apiKey = Convert.ToBase64String(key);
+            Save(apiKey);
+            return apiKey;
+        }
+        private void Save(string key)
+        {
+            var cnnStr = _config.GetSection("MyConnStr").Value;
+            using (IDbConnection conn = new SqlConnection(cnnStr))
+            {
+                string query = @$"INSERT INTO [ApiKey] (ApiKeyValue) values ('{key}');";
+                conn.Execute(query);
+            }
+        }
+
+        public bool Matches(string potentialKey)
+        {
+            var cnnStr = _config.GetSection("MyConnStr").Value;
+            using (IDbConnection conn = new SqlConnection(cnnStr))
+            {
+                var query = @$"SELECT ApiKeyValue FROM [ApiKey] WHERE ApiKeyValue = '{potentialKey}';";
+                var result = conn.Query<string>(query);
+                return result.AsList().Count == 1;
+            }
+        }
+    }
+}
---
+++ DataLibrary/Services/IApiKeyManager.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLibrary.Services
+{
+    public interface IApiKeyManager
+    {
+        string GenerateKey();
+        bool Matches(string potentialKey);
+    }
+}
